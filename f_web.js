298ea7e50984b5ad5f72c7b07a0fc5e6308724e5
LAl4l4 <147840415+LAl4l4@users.noreply.github.com>
2025-08-13 15:46:31 +0800

    Add files via upload

---
+++ f_web.js
@@ -0,0 +1,9 @@
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) { // 如果页面来自缓存
+        const btn = document.querySelector('.btn');
+        
+        btn.classList.remove("btn");
+        void btn.offsetWidth;
+        btn.classList.add("btn");
+    }
+});
